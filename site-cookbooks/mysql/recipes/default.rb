108e3635f931d43bbb4599d1e97e2a6834e65c1d
uchiunyo <tataran.jukabu@gmail.com>
2013-05-01 18:10:25 +0900

    add zabbix db import

--- site-cookbooks/mysql/recipes/default.rb
+++ site-cookbooks/mysql/recipes/default.rb
@@ -14,11 +14,6 @@ package "mysql-server" do
   action :install
 end
 
-service "mysqld" do
-  supports :status => true, :restart => true
-  action [:enable, :start]
-end
-
 template "my.cnf" do
   path "/etc/my.cnf"
   source "my.cnf.erb"
@@ -26,4 +21,9 @@ template "my.cnf" do
   group "root"
   mode 0644
   notifies :restart, 'service[mysqld]'
+end
+
+service "mysqld" do
+  supports :status => true, :restart => true
+  action [:enable, :start]
 end
--- site-cookbooks/zabbix-server/recipes/default.rb
+++ site-cookbooks/zabbix-server/recipes/default.rb
@@ -17,3 +17,36 @@ when "centos"
     action :add
   end
 end
+
+%w{zabbix-server-mysql zabbix-web-mysql}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+bash "importdb" do
+  version_tmp = `/usr/sbin/zabbix_server --version`
+  exp = Regexp.new('([0-9]+\.[0-9]+\.[0-9]+)')
+  version = exp.match(version_tmp)[0]
+
+  code <<-EOC
+    /usr/bin/mysql -uroot zabbix < /usr/share/doc/zabbix-server-mysql-#{version}/create/schema.sql
+    /usr/bin/mysql -uroot zabbix < /usr/share/doc/zabbix-server-mysql-#{version}/create/images.sql
+    /usr/bin/mysql -uroot zabbix < /usr/share/doc/zabbix-server-mysql-#{version}/create/data.sql
+  EOC
+  action :nothing
+end
+
+execute "createuser" do
+  command "/usr/bin/mysql -uroot -e \"grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbix';\""
+  action :nothing
+  notifies :run, resources( :bash => "importdb")
+end
+
+execute "createdb" do
+  not_if "/usr/bin/mysql -uroot -e 'show databases;' | grep zabbix"
+  command "/usr/bin/mysql -uroot -e 'create database zabbix character set utf8;'"
+  notifies :run, resources( :execute => "createuser")
+end
+
+
